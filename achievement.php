dc0fcb845c1f54a84a0d55793cf123a83e783d7b
nadirahikmahfatheena <116608692+nadirahikmahfatheena@users.noreply.github.com>
2022-10-25 13:53:11 +0800

    Create achievement.php

---
+++ achievement.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="mt-44">
+            <section id="bagian0" class="sectionSize bg-black text-white text-center">
+                <div class="flex w-full flex-col md:flex-row gap-3 pb-10">
+                    <div class="flex-1 flex flex-col mx-6 relative rounded-2xl px-8">
+                        <img src="assets/img/putih_2.png" width="300px" alt="">
+                        <div>
+                            <h1 class="font-bold">HALAL CERTIFICATION</h1>
+                            <br>
+                            <p>5% coffee strengten its commitment to
+                                serve the best quality product and become the first Kopi Susu
+                                Kekinian to obtain Halal Certification from Majelis Ulama Indonesia</p>
+                        </div>
+                    </div>
+
+                    <div class="flex-1 flex flex-col mx-6 relative rounded-2xl px-8">
+                        <div class="mx-auto py-8">
+                            <img src="assets/img/putih_3.png" width="140px" class="" alt="">
+                        </div>
+                        <div class="">
+                            <h1 class="font-bold">COFFEE JOURNEY</h1>
+                            <br>
+                            <p>Since 2017, it always has been and will always be about quality.
+                                We work relentlessly to guarantee we deliver on our promise. From harvest, tasting,
+                                roasting and working together with the producers in Indonesia to produce the best crop to our customers.
+                                Our passion for coffee is rivaled only by our love of sharing it.</p>
+                        </div>
+                    </div>
+
+                    <div class="flex-1 flex flex-col mx-6 relative rounded-2xl px-8 ">
+                        <img src="assets/img/putih_1.png" width="300px" alt="">
+                        <div>
+                            <h1 class="font-bold">WORLD BEST BRAND WINNER</h1>
+                            <br>
+                            <p>5% has been appointed as the only Brand of The Year winner
+                                in the Café Chain category from Indonesia by The World Branding Awards.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+</body>
+</html>
